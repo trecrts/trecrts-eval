88c98a772dd6205fe0654d8cb31a6c116c6dcaff
Adam Roegiest <aroegies@uwaterloo.ca>
2016-05-11 11:12:44 -0400

    Correcting the platform screwup additionally

--- trecrts-server/app.js
+++ trecrts-server/app.js
@@ -48,7 +48,7 @@ app.use(function(req,res,next){
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
